3014fcc984be0545636bcfb409608d858708a715
Ashley <ashley.beeson@qa.com>
2017-09-29 12:32:35 +0100

    Seconnd commit startin to remove hard coded values

--- PVP_Game/Assets/TileMap.cs
+++ PVP_Game/Assets/TileMap.cs
@@ -6,7 +6,10 @@ using UnityEngine;
 [RequireComponent(typeof(MeshRenderer))]
 [RequireComponent(typeof(MeshCollider))]
 public class TileMap : MonoBehaviour {
- 
+	
+	int sizeX = 100;
+	int sizeZ = 50;
+	float tileSize = 1.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -15,35 +18,24 @@ public class TileMap : MonoBehaviour {
 	
 	void BuildMesh(){
 
+		int numTiles = sizeX * sizeZ;
+		int numTris = numTiles * 2;
+		int vSizeX = sizeX + 1;
+		int vSizeZ = sizeZ + 1;
+		int numVerts = vSizeX * vSizeZ; 
 		// Create Mesh Data
-		Vector3[] vertices = new Vector3[4];
-		int [] triangles = new int[6];
-		Vector3[] normals = new Vector3[4];
-		Vector2[] uv = new Vector2[4];
+		Vector3[] vertices = new Vector3[numVerts];
+		Vector3[] normals = new Vector3[numVerts];
+		Vector2[] uv = new Vector2[numVerts];
 
-		vertices[0] = new Vector3(0,0,0);
-		vertices[1] = new Vector3(1,0,0);
-		vertices[2] = new Vector3(0,0,-1);
-		vertices[3] = new Vector3(1,0,-1);
+		int [] triangles = new int[numTris * 3];
 
-		triangles[0] = 0;
-		triangles[1] = 3;
-		triangles[2] = 2;
-		triangles[3] = 0;
-		triangles[4] = 1;
-		triangles[5] = 3;
-
-		normals[0] = Vector3.up;
-		normals[1] = Vector3.up;
-		normals[2] = Vector3.up;
-		normals[3] = Vector3.up;
-	
-		uv[0] = new Vector2(0,1);
-		uv[1] = new Vector2(1,1);
-		uv[2] = new Vector2(0,0);
-		uv[3] = new Vector2(1,0);
-
-
+		int x,z;
+		for(z = 0; z < sizeZ; z++){
+			for(x = 0; x <sizeX; x++){
+				vertices[z * vSizeX + x] = new Vector3(x * tileSize,0,z * tileSize);
+			}
+		}
 
 		// Create a new Mesh
 		Mesh mesh = new Mesh();
